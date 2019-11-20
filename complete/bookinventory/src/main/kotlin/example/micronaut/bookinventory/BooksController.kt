9b41c1c1b86871d759eb31cd2f4254319941eedf
Iván López <lopezi@objectcomputing.com>
2019-11-20 11:27:56 +0100

    Minor cleanup

--- complete/bookinventory/src/main/kotlin/example/micronaut/bookinventory/BooksController.kt
+++ complete/bookinventory/src/main/kotlin/example/micronaut/bookinventory/BooksController.kt
@@ -4,16 +4,14 @@ import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
-import io.micronaut.validation.Validated
+import java.util.*
 import javax.validation.constraints.NotBlank
-import java.util.Optional
 
-@Validated // <1>
-@Controller("/books") // <2>
+@Controller("/books") // <1>
 open class BooksController {
 
-    @Produces(MediaType.TEXT_PLAIN) // <3>
-    @Get("/stock/{isbn}") // <4>
+    @Produces(MediaType.TEXT_PLAIN) // <2>
+    @Get("/stock/{isbn}") // <3>
     open fun stock(@NotBlank isbn: String): Boolean? {
         return bookInventoryByIsbn(isbn).map { (_, stock) -> stock > 0 }.orElse(null)
     }
@@ -21,7 +19,6 @@ open class BooksController {
     private fun bookInventoryByIsbn(isbn: String): Optional<BookInventory> {
         if (isbn == "1491950358") {
             return Optional.of(BookInventory(isbn, 4))
-
         } else if (isbn == "1680502395") {
             return Optional.of(BookInventory(isbn, 0))
         }
